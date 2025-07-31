9b2095c6f12661b4c951f785808ad78eaae74c07
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2025-07-30 23:47:24 -0400

    Cost-Optimization: - Mocked backed API call for fetching candidate data while in  testing/development.

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -1,21 +1,38 @@
-import { FileWithUrl, PaginatedData } from "./types";
+import { FileWithUrl, JdResponseData, PaginatedData } from "./types";
 
 export const fetchResultsData = async (blobName: string) => {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/jd/results`,
+  // try {
+  //   const response = await fetch(
+  //     `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/jd/results`,
+  //     {
+  //       method: "POST",
+  //       headers: { "Content-Type": "application/json" },
+  //       body: JSON.stringify({ blobName }),
+  //       cache: "force-cache",
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   return data;
+  // } catch (error) {
+  //   console.error("Error fetching candidate data:", error);
+  // }
+  return {
+    jdUrl: "",
+    resumeResults: [
       {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ blobName }),
-        cache: "force-cache",
-      }
-    );
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching candidate data:", error);
-  }
+        fileName: "Jani Syed Resume(Java Full Stack) (1).docx",
+        fileUrl:
+          "https://jobresume.blob.core.windows.net/resumes/Jani%20Syed%20Resume%28Java%20Full%20Stack%29%20%281%29.docx?sv=2025-05-05&se=2025-08-01T03%3A30%3A51Z&sr=b&sp=r&sig=fThhvF6FyR40NqoNyOKAuhWg17Wlxr8d9y1uklt3qu8%3D&rscd=inline",
+        score: "0.85526824",
+      },
+      {
+        fileName: "Dheeraj_Krishna_Resume.pdf",
+        fileUrl:
+          "https://jobresume.blob.core.windows.net/resumes/Dheeraj_Krishna_Resume.pdf?sv=2025-05-05&se=2025-08-01T03%3A30%3A51Z&sr=b&sp=r&sig=KLtCGb5stb1G3VjxyTTJCIB%2Bm3hK8W526dRCVMTjclo%3D&rscd=inline",
+        score: "0.84172416",
+      },
+    ],
+  } as JdResponseData;
 };
 
 export const fetchResumeData = async (pageNo: string, pageSize: string) => {
