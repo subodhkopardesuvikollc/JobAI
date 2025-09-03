5c57b40b32f498a27ec825e30db0c8dee9011676
Dheeraj Krishna Nagula <dheerajkrishna141@gmail.com>
2025-09-02 20:10:13 -0400

    created an endpoint for debugging environment vairables, and axios instance

---
+++ src/app/api/debug/route.ts
@@ -0,0 +1,13 @@
+import axiosInstance, { debugAxiosInstance } from "@/utils/axios";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const debugInfo = {
+    message: "Debugging Environment Variables",
+    apiBaseUrlExists: !!process.env.API_BASE_URL,
+    apiKeyExists: !!process.env.API_KEY,
+    apiKeyLength: process.env.API_KEY ? process.env.API_KEY.length : 0,
+    axiosInstance: axiosInstance().defaults.headers,
+  };
+  return NextResponse.json(debugInfo);
+}
